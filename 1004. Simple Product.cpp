b3594cc0e5651cc2819dfce5b99acb1bc50ccef5
Md. Ahanaf Hossain <iammdahanaf@gmail.com>
2022-10-02 18:12:25 +0600

    Added 1004

---
+++ 1004. Simple Product.cpp
@@ -0,0 +1,13 @@
+// Bee 1004
+#include <stdio.h>
+using namespace std;
+ 
+int main() {
+ 
+    int a, b, PROD;
+
+    cin>>a>>b;
+    PROD = a * b;
+    cout<< "PROD = " << PROD << endl;
+    return 0;
+}
