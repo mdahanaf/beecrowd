26f09b173831b95f5657ea94b544de1a1167deae
Md. Ahanaf Hossain <iammdahanaf@gmail.com>
2022-10-02 13:34:15 +0600

    Added 1002 and 1003

---
+++ 1002.cpp
@@ -0,0 +1,15 @@
+// Bee 1002
+#include <iostream>
+#include <iomanip>
+using namespace std;
+ 
+int main() {
+ 
+double R, A, pi = 3.14159;
+cin>>R;
+
+A = pi * R * R;
+cout<<fixed<<setprecision(4)<<"A="<<A<<endl;
+
+return 0;
+}
