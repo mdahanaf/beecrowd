331e5f0b63881ecd67e28e25b5c3a3fd744c3559
Md. Ahanaf Hossain <iammdahanaf@gmail.com>
2022-10-02 13:19:12 +0600

    changes it

--- 1759. Ho Ho Ho.cpp
+++ 1759. Ho Ho Ho.cpp
@@ -1,11 +1,9 @@
 //Bee 1759
-<<<<<<< HEAD
-=======
 //I have updated this to git "developer" branch.
 //Thanks you github
 //This is from testing branch.
 
->>>>>>> 52fcccd407b8588b470c070b00e38d090589dab5
+
 
 #include <iostream>
 using namespace std;
